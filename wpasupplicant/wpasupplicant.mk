7573f1b6b594546e4c54a7bc26ad4190a9205504
Ben O'Neill <ben@benoneill.xyz>
2020-06-16 06:46:37 -0400

    Fix libnl3 and wpasupplicant

--- wpasupplicant/wpasupplicant.mk
+++ wpasupplicant/wpasupplicant.mk
@@ -14,7 +14,7 @@ wpasupplicant:QV:
 	export CFLAGS="$CFLAGS $DEPS_CFLAGS"
 	export LDFLAGS="$LDFLAGS $DEPS_LDFLAGS"
 	# patch linux_wext.h (may use -D instead).
-	sed -i 's/typedef __/typedef /g' src/drivers/linux_wext.h
+	#sed -i 's/typedef __/typedef /g' src/drivers/linux_wext.h
 	cp ../dot.config wpa_supplicant/.config
-	# add -lz as extra libs.
-	make -j$nprocs -C wpa_supplicant EXTRALIBS="-lz"
+	cd wpa_supplicant
+	make -j$nprocs
