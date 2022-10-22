0ad32a3612c4c09c4310385e70e54ea6dd57882b
Ben O'Neill <ben@oneill.sh>
2022-10-22 13:53:18 -0400

    gmp: fix pkg compilation

--- gmp/gmp.mk
+++ gmp/gmp.mk
@@ -2,14 +2,13 @@ TARG = gmp
 
 <$mkbuild/mk.common-noinst
 
-gmp:QV:
+gmp:
 	CC="$CC" ./configure \
 		--host="${HOST_TOOLCHAIN_TRIPLET}" \
 		--prefix="$PREFIX" \
 		--disable-shared \
 		--enable-static
 	make -j$nprocs
-	rm -f .libs/libgmp.la .libs/libgmp.lai
 
-install:QV:
+install:
 	make -j$nprocs install DESTDIR="$ROOT"
